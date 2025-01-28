92e80e72a84c54f4faa4e1bf4470f1d79f9623ab
dhengghuring <wyvrendragon@gmail.com>
2025-01-29 00:18:33 +0700

    feat(member): add event page
    - Implement event list with tabs
    - Add dummy event data
    - Style event cards
    - Integrate with Navbar component

---
+++ src/app/member/event/data.ts
@@ -0,0 +1,29 @@
+export const eventsDummy = [
+  {
+    id: 1,
+    category: "all",
+    title: "JCC S1 - SD/Mi sederajat",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "23 Feb 2025",
+    time: "09.00 - 10.00 PM",
+    color: "bg-red-500",
+  },
+  {
+    id: 2,
+    category: "invoice",
+    title: "JCC S1 - SD/Mi sederajat",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "23 Feb 2025",
+    time: "09.00 - 10.00 PM",
+    color: "bg-yellow-500",
+  },
+  {
+    id: 3,
+    category: "my-event",
+    title: "JCC S1 - SD/Mi sederajat",
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    date: "23 Feb 2025",
+    time: "09.00 - 10.00 PM",
+    color: "bg-green-500",
+  },
+];
