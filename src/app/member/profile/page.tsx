1c47f316637e619fab46e579676baffb01439584
dhengghuring <wyvrendragon@gmail.com>
2025-01-29 11:38:53 +0700

    feat(member): enhance profile page UI
    - Add email, name, and other fields
    - Improve layout and styling
    - Add icons for better readability

--- src/app/member/profile/page.tsx
+++ src/app/member/profile/page.tsx
@@ -3,11 +3,13 @@ import Navbar from "@/components/module/Navbar";
 import { navbarMenuMember } from "@/data/navbarMember";
 import React, { useEffect, useState } from "react";
 import { AiFillEdit } from "react-icons/ai";
-import { MdHomeFilled, MdOutlineFamilyRestroom } from "react-icons/md";
+import { MdHomeFilled, MdOutlineEmail, MdOutlineFamilyRestroom, MdOutlineSportsSoccer } from "react-icons/md";
 import { PiStudentFill } from "react-icons/pi";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import { CgGenderFemale } from "react-icons/cg";
-import { FaPhone } from "react-icons/fa";
+import { FaBook, FaPhone, FaRegUser, FaRunning } from "react-icons/fa";
+import { CiUser } from "react-icons/ci";
+import { GiMusicalNotes } from "react-icons/gi";
 
 const Profile = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -31,7 +33,7 @@ const Profile = () => {
       {/* main */}
       <div className="grid grid-cols-1 gap-4 px-4">
         {/* header */}
-        <div className="bg-base-green rounded-lg p-4">
+        <div className="rounded-lg bg-base-green p-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-2">
               <div>
@@ -47,7 +49,7 @@ const Profile = () => {
                 <p className="text-lg font-bold">50</p>
                 <p className="text-sm">poin</p>
               </div>
-              <button type="button" className="text-base-green inline-flex h-fit items-center rounded-full bg-white p-3 text-center">
+              <button type="button" className="inline-flex h-fit items-center rounded-full bg-white p-3 text-center text-base-green">
                 <AiFillEdit />
               </button>
             </div>
@@ -56,60 +58,123 @@ const Profile = () => {
 
         {/* main */}
         <div className="rounded-lg bg-white p-4">
-          <p className="mb-4 font-semibold text-neutral-700">Biodata</p>
-          <div className="grid grid-cols-1 gap-5">
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <BsCalendar2DateFill />
+          {/* biodata */}
+          <div>
+            <p className="mb-4 font-semibold text-neutral-700">Biodata</p>
+            <div className="grid grid-cols-1 gap-5 ps-5">
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <MdOutlineEmail />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">email</p>
+                  <p className="font-medium text-neutral-700">hIb0A@example.com</p>
+                </div>
               </div>
-              <div>
-                <p className="text-sm italic text-gray-400">tanggal lahir</p>
-                <p className="font-medium text-neutral-700">Banyuwangi, 9 Desember 2006</p>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <FaRegUser />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">nama</p>
+                  <p className="font-medium text-neutral-700">Andre Pratama</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <BsCalendar2DateFill />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">tanggal lahir</p>
+                  <p className="font-medium text-neutral-700">Banyuwangi, 9 Desember 2006</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <PiStudentFill />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">asal sekolah</p>
+                  <p className="font-medium text-neutral-700">SMAN 1 Genteng, kelas 11</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <MdHomeFilled />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">alamat</p>
+                  <p className="font-medium text-neutral-700">Genteng Gang Garuda No 2</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <CgGenderFemale />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">jenis kelamin</p>
+                  <p className="font-medium text-neutral-700">Perempuan</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <FaPhone />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">nomer hp</p>
+                  <p className="font-medium text-neutral-700">085231796284</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <MdOutlineFamilyRestroom />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">orang tua</p>
+                  <p className="font-medium text-neutral-700">Pak John, Bu Wati</p>
+                </div>
               </div>
             </div>
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <PiStudentFill />
+          </div>
+          {/* minat bakat */}
+          <div className="mt-10">
+            <p className="mb-4 font-semibold text-neutral-700">Minat & Bakat</p>
+            <div className="grid grid-cols-1 gap-5 ps-5">
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <GiMusicalNotes />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">Minat</p>
+                  <p className="font-medium text-neutral-700">Musik, Seni Lukis</p>
+                </div>
               </div>
-              <div>
-                <p className="text-sm italic text-gray-400">asal sekolah</p>
-                <p className="font-medium text-neutral-700">SMAN 1 Genteng, kelas 11</p>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <FaRunning />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">Bakat</p>
+                  <p className="font-medium text-neutral-700">Lari Jarak Pendek, Menulis</p>
+                </div>
               </div>
-            </div>
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <MdHomeFilled />
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <MdOutlineSportsSoccer />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">Prestasi</p>
+                  <p className="font-medium text-neutral-700">Juara 2 Lomba Lari 100m Tingkat Kabupaten</p>
+                </div>
               </div>
-              <div>
-                <p className="text-sm italic text-gray-400">alamat</p>
-                <p className="font-medium text-neutral-700">Genteng Gang Garuda No 2</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <CgGenderFemale />
-              </div>
-              <div>
-                <p className="text-sm italic text-gray-400">jenis kelamin</p>
-                <p className="font-medium text-neutral-700">Perempuan</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <FaPhone />
-              </div>
-              <div>
-                <p className="text-sm italic text-gray-400">nomer hp</p>
-                <p className="font-medium text-neutral-700">085231796284</p>
-              </div>
-            </div>
-            <div className="flex items-center gap-3">
-              <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
-                <MdOutlineFamilyRestroom />
-              </div>
-              <div>
-                <p className="text-sm italic text-gray-400">orang tua</p>
-                <p className="font-medium text-neutral-700">Pak John, Bu Wati</p>
+              <div className="flex items-center gap-3">
+                <div className="h-fit w-fit rounded-full bg-teal-100 p-3 text-lg text-teal-500">
+                  <FaBook />
+                </div>
+                <div>
+                  <p className="text-sm italic text-gray-400">Hobi</p>
+                  <p className="font-medium text-neutral-700">Membaca Novel, Menggambar</p>
+                </div>
               </div>
             </div>
           </div>
